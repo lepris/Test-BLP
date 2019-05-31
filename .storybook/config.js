f773fa0de6435871cc6fb951fc2a5eadfc3cabba
Lepris <karolina.jankowska@gmail.com>
2019-05-31 20:29:26 +0100

    Storybook info-addon integrated

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,15 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+addDecorator(
+  withInfo({
+    header: true, // Global configuration for the info addon across all of your stories.
+  })
+);
 
 function loadStories() {
-  require('../src/stories');
-}
+  require('../src/stories')
 
+}
 configure(loadStories, module);
+
