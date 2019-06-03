99213d57546d8ee48a7c146fcffd623e68b5ba51
Karolina Jankowska <karolina.jankowska@gmail.com>
2019-06-03 22:00:41 +0100

    Task Card left column

--- src/components/OTaskCard.jsx
+++ src/components/OTaskCard.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import '../styles/OTaskCard.css'
 class TaskCard extends Component {
+    
     state = {  }
     render() { 
+        const {
+            procName,
+            procDescription,
+            assigned,
+            priority,
+            workers,
+            timeRemaining,
+            avgTime,
+            tasksTotal,
+            tasksQueue,
+            } = this.props;
         return ( 
-            <div className='task-card-wrapper'>
-                
+            <div className='task-card-wrapper frame-box-shadow'>
+                <div className='task-first-column teal-border'>
+                    <label className=' process-name-label label-text text-inner-shadow'>Name</label>
+                    <h3 className='process-name '>{procName}</h3>
+                    <span>{procDescription}</span>
+                </div>
+                <div className='task-second-column'>
+                </div>
             </div>
          );
     }
