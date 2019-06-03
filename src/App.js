aa1134f55973d21fd78eb7bda78862067e2446ad
Karolina Jankowska <karolina.jankowska@gmail.com>
2019-06-03 17:51:38 +0100

    Adding Layout to the App with mock bg

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import "./App.css";
 class App extends Component {
 
   render() {
-    return <div className="App" />;
+    return (
+    
+    <div className='App' >
+      <div className='main-column'>
+        <nav>
+
+        </nav>
+        <div className='task-list'>
+        </div>
+      </div>
+
+    </div>
+    )
   }
 }
 
