82bb99d90d2b4f97d901a25fad5060fa4b65cb80
Karolina Jankowska <karolina.jankowska@gmail.com>
2019-06-03 17:54:13 +0100

    Added Task Card Mock and Wireframe

---
+++ src/components/OTaskCard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/OTaskCard.css'
+class TaskCard extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className='task-card-wrapper'>
+
+            </div>
+         );
+    }
+}
+ 
+export default TaskCard;
