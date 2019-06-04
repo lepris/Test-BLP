924d21fd98a346bfbf67dfbdbdb66756638230cc
Lepris <karolina.jankowska@gmail.com>
2019-06-04 19:11:57 +0100

    NavBar padding added

--- src/components/ONavBar.jsx
+++ src/components/ONavBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/ONavBar.css';
-import {MenuIcon, RefreshIcon} from '../Icons/AllIcons';
+import { MenuIcon, RefreshIcon } from '../Icons/AllIcons';
 import SearchForm from './SearchForm';
 const ONavBar = () => {
-    return ( 
+    return (
         <div className='nav-bar-wrapper prism-border'>
             <div className='nav-first-column column'>
                 <MenuIcon />
@@ -15,7 +15,7 @@ const ONavBar = () => {
                 <RefreshIcon />
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default ONavBar;
