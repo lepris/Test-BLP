945de4a2e6f0212d13fa8d8c59a7dd1ec6150061
Lepris <karolina.jankowska@gmail.com>
2019-06-04 20:36:17 +0100

    added styles and hover

--- src/components/AssignDropMenu.jsx
+++ src/components/AssignDropMenu.jsx
@@ -1,21 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import '../styles/AssignDropMenu.css'
+const AssignDropMenu = () => (
 
-const AssignDropMenu = ({ }) => (
-    <div className='drop-menu-wrapper'>
-        <div className='drop-menu-item prism-border'>
+    <div className='drop-menu-wrapper '>
+
+        <div className='drop-menu-item first'>
             Auto-assign
         </div>
-        <div className='drop-menu-item prism-border'>
+        <div className='drop-menu-item clickable border-left-right'>
             Select workers
         </div>
-        <div className='drop-menu-item prism-border'>
+        <div className='drop-menu-item clickable border-left-right'>
             Schedule
         </div>
-        <div className='drop-menu-item prism-border'>
+        <div className='drop-menu-item clickable last'>
             Archive
         </div>
     </div>
+
 );
 
 AssignDropMenu.propTypes = {};
