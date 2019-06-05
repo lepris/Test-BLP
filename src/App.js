6c50343023c4bb86853df254a4cead5017fc28e8
Lepris <karolina.jankowska@gmail.com>
2019-06-05 13:05:00 +0100

    Added app logic and sample data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,43 @@ import React, { Component } from "react";
 import "./App.css";
 import ONavBar from './components/ONavBar';
 import OTaskCard from './components/OTaskCard';
+import { newProcesses } from './data/sample_processes';
 class App extends Component {
   state = {
-    tasks: [1, 2, 3]
+    workersTotal: 25
   }
+  componentDidMount() {
+    this.setState({ processes: newProcesses })
+  }
+  setActive = (processId) => {
+    const { processes } = this.state;
+    let tempProcess = processes.map(proc =>
+      proc.procId === processId ? { ...proc, assigned: true } : proc
+    )
+    this.setState({ processes: tempProcess })
+  }
+  stopActive = (processId) => {
+    const { processes } = this.state;
+    let tempProcess = processes.map(proc =>
+      proc.procId === processId ? { ...proc, assigned: false } : proc
+    )
+    this.setState({ processes: tempProcess })
+  }
+  setTaskPriority = (newPriority, processId) => {
+    console.log('app set priority', newPriority, processId)
+    const { processes } = this.state;
+    let tempProcess = processes.map(proc =>
+      proc.procId === processId ? { ...proc, priority: +newPriority } : proc
+    )
+    console.log(tempProcess)
+    this.setState({ processes: tempProcess })
+  }
+
+
   render() {
-    const { tasks } = this.state;
+    let { processes } = this.state;
+
+
     return (
 
       <div className='App' >
@@ -18,7 +49,7 @@ class App extends Component {
 
 
         <div className='middle'>
-          {tasks.map(task => <OTaskCard />)}
+          {processes && processes.map(proc => <OTaskCard setTaskPriority={this.setTaskPriority} stopActive={this.stopActive} setActive={this.setActive} key={proc.procId} {...proc} />)}
 
 
 
---
+++ src/data/sample_processes.js
@@ -0,0 +1,26 @@
+export const newProcesses = [
+    {
+        procId: 1,
+        procName: 'Stock for the new cafe in Manchester',
+        procDescription: 'Choose coffee, order coffee, pay VAT, buy coffee maker',
+        assigned: false,
+        priority: 0,
+        workers: 0,
+        timeRemaining: '2h',
+        avgTime: '10m',
+        tasksTotal: 40,
+        tasksQueue: 40,
+    },
+    {
+        procId: 2,
+        procName: 'Find and recruit new staff members',
+        procDescription: 'post job offers, check submissions, send e-mails, set dates',
+        assigned: false,
+        priority: 0,
+        workers: 0,
+        timeRemaining: '2h',
+        avgTime: '10m',
+        tasksTotal: 12,
+        tasksQueue: 12,
+    }
+]
